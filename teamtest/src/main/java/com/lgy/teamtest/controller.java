0611072fd87cfdf4102f62433dc247d6d1f86220
park-chanhyung <chanhyung13@gmail.com>
2024-01-18 17:39:21 +0900

    teamtest 2024-01-18 찐찐찐

---
+++ teamtest/src/main/java/com/lgy/teamtest/controller.java
@@ -0,0 +1,13 @@
+package com.lgy.teamtest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class controller {
+	@RequestMapping("/index")
+	public String login() {
+		
+		return "index";
+	}
+	
+}
