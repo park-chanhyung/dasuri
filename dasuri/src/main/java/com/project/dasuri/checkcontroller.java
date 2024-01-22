720e983f8a01fce6f7084ce397394353a93d41d4
deadline000 <ssa7119@naver.com>
2024-01-22 16:28:53 +0900

    빡통의 노력2

--- dasuri/src/main/java/com/project/dasuri/checkcontroller.java
+++ dasuri/src/main/java/com/project/dasuri/checkcontroller.java
@@ -10,5 +10,28 @@ public class checkcontroller {
 
         return "index";
     }
+    //  관리자 페이지 : 홈
+    @RequestMapping("/a")
+    public String admin_home() {
 
+        return "admin_home";
+    }
+    //  관리자 페이지 : 회원관리
+    @RequestMapping("/admin_mem")
+    public String admin_mem() {
+
+        return "admin_mem";
+    }
+    //  관리자 페이지 : 공지관리
+    @RequestMapping("/admin_notice")
+    public String admin_notice() {
+
+        return "admin_notice";
+    }
+    //  관리자 페이지 : 고객센터
+    @RequestMapping("/admin_moon")
+    public String admin_moon() {
+
+        return "admin_moon";
+    }
 }
