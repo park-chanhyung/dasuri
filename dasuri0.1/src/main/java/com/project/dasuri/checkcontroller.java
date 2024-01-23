49fd30a444165346987d2fa6e88536966ea306d4
DESKTOP-LG2S9LN\김준우 <kwj7585@gmail.com>
2024-01-23 13:12:29 +0900

    다정씨 저녁작업까지 합침

--- dasuri0.1/src/main/java/com/project/dasuri/checkcontroller.java
+++ dasuri0.1/src/main/java/com/project/dasuri/checkcontroller.java
@@ -11,57 +11,86 @@ public class checkcontroller {
 
         return "index";
     }
+
+    @RequestMapping("/notice")
+    public String notice() {
+//    public String index() {
+
+        return "/list/notice";
+    }
+
+    @RequestMapping("/community")
+    public String community() {
+//    public String index() {
+
+        return "/list/community";
+    }
+
+    @RequestMapping("/proinfo")
+    public String proinfo() {
+//    public String index() {
+
+        return "/list/proinfo";
+    }
+
+    @RequestMapping("/servicecenter")
+    public String servicecenter() {
+//    public String index() {
+
+        return "/list/servicecenter";
+    }
+
     //  관리자 페이지 : 홈
-    @RequestMapping("/a")
+    @RequestMapping("/admin")
     public String admin_home() {
 
-        return "admin_home";
+        return "/adminad/admin_home";
     }
     //  관리자 페이지 : 회원관리
     @RequestMapping("/admin_mem")
     public String admin_mem() {
 
-        return "admin_mem";
+        return "/adminad/admin_mem";
     }
     //  관리자 페이지 : 공지관리
     @RequestMapping("/admin_notice")
     public String admin_notice() {
 
-        return "admin_notice";
+        return "/adminad/admin_notice";
     }
     //  관리자 페이지 : 고객센터
     @RequestMapping("/admin_moon")
     public String admin_moon() {
 
-        return "admin_moon";
+        return "/adminad/admin_moon";
     }
     @RequestMapping("/login")
     public String login2() {
 
-        return "login";
+        return "/login/login";
     }
 
     @RequestMapping("/register")
     public String register() {
 
-        return "register";
+        return "/login/register";
     }
 
 
     @RequestMapping("/register_expert")
     public String register_expert() {
 
-        return "register_expert";
+        return "/login/register_expert";
     }
 
     @RequestMapping("/find_user")
     public String find_user() {
 
-        return "find_user";
+        return "/login/find_user";
     }
     @RequestMapping("/my_profile")
     public String my_profile() {
 
-        return "my_profile";
+        return "/login/my_profile";
     }
 }
