01dc901311398fad8b8696e09582de3d1d955dcf
park-chanhyung <133179643+park-chanhyung@users.noreply.github.com>
2024-02-08 09:47:26 +0900

    Delete dasuri0.1 directory

--- dasuri0.1/src/main/java/com/project/dasuri/member/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.dasuri.member.repository;
-
-import com.project.dasuri.member.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-//    Optional<UserEntity> findByUserId(String userId);
-}
